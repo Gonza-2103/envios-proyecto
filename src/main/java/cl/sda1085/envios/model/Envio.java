68f75cde0d9e50d6df3034a08d3229e179480227
Gonza-2103 <ghormazabal.d@gmail.com>
2026-05-06 12:41:38 -0400

    Elaboración de 'model'

--- src/main/java/cl/sda1085/envios/model/Envio.java
+++ src/main/java/cl/sda1085/envios/model/Envio.java
@@ -1,5 +1,32 @@
 package cl.sda1085.envios.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "envios")
+
 public class Envio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long idSubasta;
+
+    @Column(nullable = false, length = 100)
+    private String direccion;
+
+    @Column(name = "estado_envio", nullable = false, length = 30)  //Pendiente, en camino, entregado y devuelto
+    private String estado_envio;
+
+    @Column(name = "codigo_seguimiento", unique = true, length = 15)  //Generado para que el cliente rastree su obra de arte
+    private String codigo_seguimiento;
 }
